ae81ba5dd5f7858d7cc1a8a9bd20d44b28dadf90
Ange Lowe <ange.gououi@gmail.com>
2020-08-12 11:13:33 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit");
+            Console.ReadKey(true);
+
+        }
+    }
+}
